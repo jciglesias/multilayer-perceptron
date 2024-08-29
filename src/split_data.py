df2dbcc07661ead640f3135cac49c52fc7a3376f
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-08-29 11:14:20 +0200

    neurons per layer

--- src/split_data.py
+++ src/split_data.py
@@ -3,10 +3,9 @@ import pandas as pd
 if __name__=="__main__":
     try:
         data = pd.read_csv("data/data.csv")
-
-        training = data.sample(frac=0.4).reset_index(drop=True)
-        validation = data.sample(frac=0.4).reset_index(drop=True)
-        test = data.sample(frac=0.2).reset_index(drop=True)
+        training = data.sample(frac=0.7)
+        validation = data.drop(training.index).sample(frac=0.5)
+        test = data.drop(training.index).drop(validation.index)
 
         training.to_csv("data/training.csv", index=False)
         validation.to_csv("data/validation.csv", index=False)
